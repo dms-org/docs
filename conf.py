fe7266e400eae8fc57eeba369f21eea543f84c90
Elliot Levin <elliot@iddigital.com.au>
2017-07-05 13:50:46 +1000

    Update sphinx config

--- conf.py
+++ conf.py
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
+  ('docs/index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
    u'Read the Docs', 'manual'),
 ]
 
@@ -226,7 +226,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
+    ('docs/index', 'readthedocstemplate', u'Read the Docs Template Documentation',
      [u'Read the Docs'], 1)
 ]
 
@@ -240,7 +240,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
+  ('docs/index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
    u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
